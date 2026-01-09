e4082c921b6e82934813774e9e3767c6328d778e
Raaghav-m <raaghavmanivel2005@gmail.com>
2026-01-09 12:29:06 +0530

    Initial commit

---
+++ src/models/__init__.py
@@ -0,0 +1,6 @@
+"""
+__paper__ = Beyond Prompt Content: Enhancing LLM Performance via Content-Format Integrated Prompt Optimization (CFPO)
+__author__ = Yuanye Liu#, Jiahang Xu#
+__version__ = 1.0
+__date__ = 2025-1
+"""
